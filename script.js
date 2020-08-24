ed928fe79e8e8e685b8fb3df92ed9c0f41d9d52e
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-08-24 23:49:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,81 @@
 // Fonctionnalité 1 et 1bis:
-let footerClick = 0;
-document.getElementsByTagName("footer")[0].addEventListener("click", function(){
-  footerClick ++;
-	 console.log(`Click ${footerClick}`);
-});
+let footerEl = document.querySelector("footer");
+let x = 0;
+
+function consolePrint() {
+    x++;
+    console.log(`clic numero ${x}`);
+}
+
+if (footerEl) {
+    footerEl.addEventListener("click", consolePrint);
+}
 
 //Fonctionnalité 2 :
 // 'élément HTML portant l'Id navbarHeader perde sa classe collapse
 
-let navbar = document.getElementById("navbarHeader");
-let hamburger = document.querySelector("span.navbar-toggler-icon")
-function Collapse() {
-  navbar.classList.toggle("collapse");
+let navbarEl = document.getElementById("navbarHeader");
+let hamburgerEl = document.querySelector("span.navbar-toggler-icon")
+
+function menuToogle() {
+  navbarEl.classList.toggle("collapse");
 };
 
-hamburger.addEventListener("click",Collapse);
-
+if (navbarEl) {
+    hamburgerEl.addEventListener("click", menuToogle);
+}
 
 //Fonctionnalité 3 :
-let btnEditElmt = document.querySelectorAll('.btn.btn-sm.btn-outline-secondary')[0];
+let cardEl = document.getElementsByClassName("col-md-4")[0];
+let editEl = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[0];
+let textEl = document.getElementsByClassName("card-text")[0];
 
-btnEditElmt.addEventListener('click', function() {
-  btnEditElmt.style.color = 'red';
-});
+console.log(cardEl);
+console.log(editEl);
+console.log(textEl);
+
+function changeColor(){
+    textEl.style.color = "red";
+}
+
+if (editEl){
+    editEl.addEventListener("click", changeColor);
+}
 
 
 //Fonctionnalité 4 :
 
-var second_edit_button = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[1];
+var secondEditButton = document.getElementsByClassName("btn btn-sm btn-outline-secondary")[1];
+
 var onMouseClickSecondEdit = function () {
-    var x = document.getElementsByClassName("card-text")[1];
-    if (x.style[0] === "color") {
-        x.style = " "}
-        else {
-        x.style = "color : Green";}
-    }
-second_edit_button.addEventListener("click", onMouseClickSecondEdit, false);
+  var x = document.getElementsByClassName("card-text")[1];
+  if (x.style[0] === "color") {
+    x.style = " "}
+  else {
+    x.style = "color : Green";} 
+}
+
+secondEditButton.addEventListener("click", onMouseClickSecondEdit, false);
 
 //Fonctionnalité 5 :
+let styleLinkEl = document.querySelector("link");
+let headerEl = document.querySelector("header");
+console.log(styleLinkEl);
+console.log(headerEl);
+
+let index = 0;
+
+function doubleClic() {
+    if (index == 2){
+        styleLinkEl.disabled = true;
+    }
+    else if (index == 4){
+        styleLinkEl.disabled = false;
+        index = 0;
+    }
+    index++;
+}
+
+if (headerEl){
+    headerEl.addEventListener("click", doubleClic);
+}
